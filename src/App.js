08b5c570e2f228c369722f0959ca685dfdf10510
Kay Kostadinov <kaloyan.kostadinov@anakatech.com>
2020-12-15 11:55:22 +0200

    Merging changes from the other repo

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
 import React, { useState } from 'react'
+//components
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Editor from './components/Editor';
 import ThemePicker from './components/ThemePicker';
 import ThemeNP1 from './components/ThemeNp1';
 import ThemeNP2 from './components/ThemeNp2';
+// libs
+import {debounce} from 'lodash';
 
 
 function App() {
+
+  // form state
   const [data, setData] = useState({
     name: '',
     title: '',
     phone: '',
     fax: '',
-    website: '',
+    website: {
+      name: '',
+      link: ''
+    },
     address: '',
     social: {
       fb: {
         link: '',
-        icon: 'fab fa-facebook',
+        icon: 'https://github.com/anakatech-kaykostadinov/mailsig/blob/main/src/images/filled_icons/facebook%20(1).png?raw=true'
       },
       ig: {
         link: '',
-        icon: 'fab fa-instagram',
+        icon: 'https://github.com/anakatech-kaykostadinov/mailsig/blob/main/src/images/filled_icons/instagram%20(1).png?raw=true'
       },
       linkedin: {
         link: '',
-        icon: 'fab fa-linkedin-in',
-      },
-      twitter: {
-        link: '',
-        icon: 'fab fa-twitter',
+        icon: 'https://github.com/anakatech-kaykostadinov/mailsig/blob/main/src/images/filled_icons/linkedin%20(1).png?raw=true'
       },
     }
   })
 
-  const [selected, setSelected] = useState('nm')
 
-  function copyToClipboard() {
+  const [selected, setSelected] = useState('nm');
+
+  // copy button functionality
+  const [btn, setBtn] = useState({
+    class: '',
+    message: 'Copy'
+  });
+
+  const copyToClipboard = debounce(() => {
     const table = document.getElementById('table-content');
     let range = document.createRange();
     range.selectNodeContents(table);
@@ -45,8 +56,23 @@ function App() {
     select.removeAllRanges();
     select.addRange(range);
     document.execCommand('copy');
-    select.removeRange(range)
-  };
+    select.removeRange(range);
+
+    //btn animation
+    setBtn({
+      class: 'copy-btn--success',
+      message: 'Copied to clipboard'
+    });
+    setTimeout(() => {
+      setBtn({
+        class: '',
+        message: 'Copy'
+      });
+    }, 2000);
+
+  }, 200); // debounce timeout
+
+  
 
   return (
     <div className="ms-main">
@@ -55,23 +81,24 @@ function App() {
       <main>
         <Editor data={data} setData={setData} />
         <section className="preview-col">
-        {
-          (()=> {
-            switch (selected) {
-              case 'nm':
-                return <ThemeNP1 name={selected} data={data} />
-              case 'np':
-                return <ThemeNP2 name={selected} data={data} />
-              default:
-                break;
-            }
-          })()
-        }
-          <button className="copy-btn" onClick={copyToClipboard}>
-            <i className="far fa-copy"></i> Copy Signature
+          <button className={`copy-btn ${btn.class}`} disabled={btn.class != '' ? true : false} onClick={copyToClipboard}>
+            <i className="far fa-copy"></i> {btn.message}
           </button>
+          <div className='form-container'>
+            {
+              (()=> {
+                switch (selected) {
+                  case 'nm':
+                    return <ThemeNP1 name={selected} data={data} />
+                  case 'np':
+                    return <ThemeNP2 name={selected} data={data} />
+                  default:
+                    break;
+                }
+              })()
+            }
+          </div>
         </section>
-        
       </main>
       <Footer />
     </div>
--- src/components/ThemeNp1.jsx
+++ src/components/ThemeNp1.jsx
@@ -1,155 +1,136 @@
 import React from 'react'
-import bg from '../images/nm.png'
 
-function Theme({data}) {
-    
+function Theme({data, click}) {
+
 //816px
     return (
-        <table
-            className='table'
+<table 
             id='table-content'
-            cellPadding="0"
-            cellSpacing="0"
+            className="table" 
             style={{
-                verticalAlign: '-webkit-baseline-middle',
-                fontSize: '16px',
-                fontFamily: 'Verdana',
-                padding: '1rem 5rem',
-                width: '816px',
-                maxWidth: '100%',
-                height: '234px',
-                margin: '2rem 0',
-                whiteSpace: 'nowrap',
-                backgroundImage: "url('https://lh3.googleusercontent.com/pQ9Df9YiNOgZCRA3UBSjb1-yxYc32K4PJ9TxVOsypWKmRDGAD0tUdRcM5P5JPnEq0sAXfJzr1Nc6QVI7ITFd72Bd-DWmuIBDUxcBEdA6J03HhSp83UMVqVakBebyNWPiQDv2SW2vbh810bs15eVZVXJX1-IrPZTWm-vm6gF2sfS03kP5lCDbZSdwz3VHTSmZI00BVtWfgBtdDFdtBPy6FXOco2qloKSM1zOo3BLO2ywm1TtiIHSeHQQHTF1diIcHBEVmNcvqbC43SIvsAEorN9lPNjLS7jRXLAkRw-zguuzA9Dxqedn5CzRMCbuQuoOtcA_m54VNhex6NKEucZqYdCmORdSWOxM_KcRO9SNVFBlJDYf6QB83TWLxqrFwv7DjfhetIcwX5TbBmwx3lwoGktHV5nMRKKNIEyrfmTSzwTjhrI2IDRHzAhOAPiA_p5k7xptPB9sbH3kMoORUoYE7ZASg3s1ugrdQPptnyIUFvPKLK42tztFBrs7PXeVNm6A2eG4mKQVUDaG4vFweN5l-TFgxT7jT5v-c3_LXCQu4tyg7Lw4hPGOiv94BZ9fl11EgYnWWFIGRAk4M2odZZnNEziUTIDtnTOprhQrap8o7wmn5HDIomtuSwOKzSKvxE_1RbbofSHa0svSmYdVN-NOeqL0vCSiYQGTL5BAoXWLcI_RFiWW8_nACqfXwxHU=w816-h234-no?authuser=0')",
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'contain'
-            }}
-            id="table-content"
-        >
-            <tbody>
+            padding: '0 4em', 
+            color: 'whitesmoke', 
+            background: "url('https://raw.githubusercontent.com/anakatech-kaykostadinov/mailsig/gh-pages/static/media/nm.f7d08900.png')", 
+            backgroundSize: 'cover', 
+            backgroundRepeat: 'no-repeat', 
+            width: '100vw', 
+            maxWidth: '816px',
+            fontSize: '16px',
+            height: '40vw',
+            maxHeight: '234px',
+        }}>
+            <tr>
                 <tr style={{
+                        width: '100%',
+                    }}
+                >
+                    <td style={{
+                        width: '100%',
+                        height: '.5em',
                         color: '#262626',
                         fontSize: '24px',
                         fontWeight: '600',
-                        height: '1rem'
-                    }}
-                >
-                    <td>{data.name}</td>
+                        whiteSpace: 'nowrap',
+                    }}>
+                        {data.name}
+                    </td>
                 </tr>
-                <tr style={{height: '1rem'}}>
+                <tr>
                     <td style={{
-                            color: 'teal',
-                            fontWeight: '600',
-
-                        }}
-                    >{data.title}</td>
-                </tr>
-
-                <tr style={{
-                        color: 'white',
-                        height: '3.5rem',
-                        verticalAlign: 'bottom'
-                    }}
-                >
-                    <td>
-                        {data.phone &&
-                        <React.Fragment >
-                            <i
-                                className="fas fa-phone-alt"
-                                style={{
-                                    backgroundColor: 'lightblue',
-                                    borderRadius: '50%',
-                                    fontSize: '.7rem',
-                                    padding: '.3rem',
-                                    color: '#262626',
-                                    marginRight: '.5rem'
-                                }}
-                            ></i>
-                            {data.phone}
-                        </React.Fragment>
-                        }
+                        width: '100%',
+                        color: 'orange',
+                        fontWeight: '600',
+                        verticalAlign: 'top',
+                    }}>
+                        {data.title}
                     </td>
                 </tr>
-
-                <tr style={{
-                        color: 'white',
-                        height: '1.5rem',
-                        verticalAlign: 'bottom'
-                    }}
-                >
-                    <td>
-                        {data.fax &&
-                        <React.Fragment>
-                            <i
-                                class="fas fa-phone-alt"
-                                style={{
-                                    backgroundColor: 'lightblue',
-                                    borderRadius: '50%',
-                                    fontSize: '.7rem',
-                                    padding: '.3rem',
-                                    color: '#262626',
-                                    marginRight: '.5rem'
-                                }}
-                            ></i>
-                            {data.fax}
-                        </React.Fragment>
-                        }
-                    </td>
-                </tr>
-
-                <tr style={{
-                        color: 'white',
-                        height: '2.5rem',
-                        verticalAlign: 'bottom'
-                    }}
-                >
-                    <td>
-                        <a
-                            style={{
-                                color: 'lightblue',
-                                fontWeight: '600',
-                                textDecoration: 'none'
+                <tr>
+                    {data.phone &&
+                        <td 
+                        style={{
+                            width: '100%', 
+                            verticalAlign: 'bottom', 
+                            height: '3em',
                             }}
-                            href={data.website}
-                        >{data.website}</a>
+                        >
+                            <img 
+                            style={{
+                                marginRight: '.5em', 
+                                marginBottom: '.15em', 
+                                width: '1.1em', 
+                                height: '1.1em', 
+                                display: 'inline-block',
+                                verticalAlign: 'bottom', 
+                                }} 
+                            src='https://github.com/anakatech-kaykostadinov/mailsig/blob/main/src/images/phone.png?raw=true' alt=""/>
+                            {data.phone}
+                        </td>
+                    }
+                </tr>
+                {data.fax &&
+                    <tr>
+                        <td style={{width: '100%', verticalAlign: 'top', display: 'flex', }}>
+                            <img style={{marginRight: '.5em', width: '1.1em', height: '1.1em', }} src='https://github.com/anakatech-kaykostadinov/mailsig/blob/main/src/images/phone.png?raw=true' alt=""/>
+                            <p style={{margin: '0', }}>{data.fax}</p>
+                        </td>
+                    </tr>
+                }
+                <tr>
+                    <td style={{width: '100%', verticalAlign: 'bottom',}}>
+                        <a 
+                        href={data.website.link}
+                        style={{
+                            textDecoration: 'none',
+                            color: 'whitesmoke',
+                            fontWeight: '600'
+                        }}
+                        >
+                            {data.website.name}
+                        </a>
                     </td>
                 </tr>
 
-                <tr style={{
-                    color: 'white',
+                <tr
+                style={{
+                    width: '100%',
                 }}
                 >
-                    <td>
-                        {data.address} 
+                    <td style={{width: '50%', verticalAlign: 'top',}}>
+                        {data.address}
                     </td>
-                    { // social media buttons
+
+                    <td style={{
+                        textAlign: 'right',
+                        width: '100%',
+                        display: 'flex',
+                    }}>
+                        { // social media buttons
                         Object.entries(data.social).map( (media, key) => {
                             // show only if field is populated
                             if (media[1].link !== '') {
                                 return (
-                                    <td key={key}
+                                    <a 
+                                        key={key}
+                                        href={media[1].link}
                                         style={{
-                                            textAlign: 'right',
-                                            width: '1rem',
-                                            padding: '0 .25rem'
+                                            margin: '0 .2em'
                                     }}
                                     >
-                                        <a href={media[1].link}>
-                                            <i 
-                                                className={media[1].icon}
-                                                style={{
-                                                    color: 'lightblue',
-                                                    fontSize: '2rem'
-                                                }}
-                                            ></i>
-                                        </a>
-                                    </td> 
+                                        <img
+                                            style={{
+                                            width: '2em',
+                                            height: '2em'
+                                        }}
+                                        src={media[1].icon}
+                                        alt="social media"/>
+                                    </a>
                                 );
                             }
-                        })
-                    }
+                        })}
+                    </td>
                 </tr>
-            </tbody>
+            </tr>
         </table>
     )
 }
@@ -157,3 +138,29 @@ function Theme({data}) {
 export default Theme
 
 
+// style={{width: '2em', height: '2em', }} 
+
+// { // social media buttons
+//     Object.entries(data.social).map( (media, key) => {
+//         // show only if field is populated
+//         if (media[1].link !== '') {
+//             return (
+//                 <a 
+//                 key={key}
+//                 href={media[1].link}
+//                 style={{
+//                     margin: '0 .2em'
+//                 }}
+//                 >
+//                     <img
+//                     style={{
+//                         width: '2em',
+//                         height: '2em'
+//                     }}
+//                     src={media[1].icon}
+//                     alt="social media"/>
+//                 </a>
+//             );
+//         }
+//     })
+// }
